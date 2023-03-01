81e13f0479efbd628631dab80204223e4c9b565a
Darren Spruell <phatbuckett@gmail.com>
2023-03-01 01:28:15 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# resolve-hosts
+Resolve list of DNS hostnames.
+
---
+++ resolve_hosts.py
@@ -0,0 +1,84 @@
+"Resolve list of DNS hostnames."
+
+import argparse
+from ipaddress import ip_address
+from json import dumps as json_dumps
+import logging
+
+from dns import query
+
+# from dns.resolver import resolve
+from dns.resolver import Resolver, resolve
+
+
+logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
+
+
+def resolve_servers(servers: list) -> list:
+    "Return IP addresses of input DNS servers, after resolving any hostnames."
+
+    results = []
+    for s in servers:
+        try:
+            ip_address(s)
+            results.append(s)
+        except ValueError:
+            r = resolve(s)
+            for addr in r:
+                results.append(str(addr))
+    return results
+
+
+def cli():
+    description = "Resolve list of DNS hostnames."
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument(
+        "infile",
+        type=argparse.FileType("r"),
+        help=("source input for list of names to resolve "
+              "(default: standard input)"),
+    )
+    parser.add_argument(
+        "-s",
+        "--server",
+        action="append",
+        help="server (DNS resolver) to query (default: use system resolver)",
+    )
+    parser.add_argument(
+        "-d", "--debug", action="store_true", help="enable debug output"
+    )
+    args = parser.parse_args()
+
+    if args.debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    logging.debug(
+        "configured to use resolver(s): %s",
+        args.server if args.server else "local system",
+    )
+
+    resolver_kwargs = {"resolvers": []}
+
+    # if args.server is not an IP, resolve it first
+    if args.server:
+        resolver_kwargs["resolvers"] = resolve_servers(args.server)
+        logging.debug(
+            "effective resolver address(es): %s",
+            resolver_kwargs["resolvers"],
+        )
+
+    resolver = Resolver()
+    if resolver_kwargs["resolvers"]:
+        resolver.nameservers = resolver_kwargs["resolvers"]
+
+    for fqdn in args.infile:
+        answer = resolver.resolve(fqdn.strip())
+        print(answer)
+    #   answer.qname
+    #   answer.rrset
+    #
+    #
+
+    # results = []
+    # for host in args.infile:
+    #    query
